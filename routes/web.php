4126aadf41d90188005864227642a4a7b3979259
Daffa-258 <ddhyaulhaq@gmail.com>
2024-08-21 16:37:27 +0700

    tugas hari-13

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@ Route::get('/',[HomeController::class,'Home']);
 Route::get('/Register',[AuthController::class,'Register']);
 Route::get('/Welcome',[AuthController::class,'Welcome']);
 
+Route::get('/table',function(){
+    return view('table');
+});
+
+route::get('/data-table',function(){
+    return view('data-table');
+});
+
